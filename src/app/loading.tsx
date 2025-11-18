3b15169acc42d9b6b8869d31b05b65d2efbe4478
Jakov C <qwerasdftzu@gmail.com>
2025-11-18 18:08:01 +0100

    Use tailwind in loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 import { Loader } from "./components/Loader/Loader";
-import styles from "./Loading.module.css";
+
 export default function LoadingPage() {
     return (
-        <div className={styles["loading-container"]}>
+        <div className="flex justify-center items-center min-h-screen overflow-hidden">
             <Loader />
         </div>
     );
